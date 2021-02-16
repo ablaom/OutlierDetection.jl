a9683ffaa973147ddf1207c3e7259fb3008415d9
Documenter.jl <documenter@juliadocs.github.io>
2021-02-16 15:16:51 +0000

    build based on 03ae742

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OutlierDetection","category":"page"},{"location":"#OutlierDetection","page":"Home","title":"OutlierDetection","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OutlierDetection]","category":"page"}]
+}
